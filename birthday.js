fad2b03854c573b28fc81b9ecda4e60f8ae84b0f
Giorvi S Alvarez <ga73724@srcs.loc>
2018-02-09 10:58:10 -0800

    test

--- birthday.js
+++ birthday.js
@@ -1,12 +1,9 @@
 var button = document.getElementById('How old am I');
 button.addEventListener('click' () => {
 
-            function getBithday() {
-                var currentDate = new Date()
-                var age = Math.floor((currentDate - this.birthdate) / 31536000000);
-                return age;
-            }
+
 
         function getSign(){
 
+}
 }
