52003ec9908275b310df37681fbd232696e03d39
Yessenia Hernandez Hernandez <yh90926@srcs.loc>
2018-02-13 11:19:19 -0800

    working on birthday countdown funtion

--- birthday.js
+++ birthday.js
@@ -60,30 +60,4 @@ button.addEventListener('click' () => {
     return ("My Birthday is in " + days+ " days :(");
 }
 
-function superiorGreeting(msg) {
-    var d = new Date();
-    var hour = d.getHours();
-    var day = d.getDay();
-    var msg = "";
-    if (hour < 12) {
-        if (day == 0 || day == 6) {
-            msg = "Go back to sleep! Its the weekend!";
-        } else if (day == 1) {
-            msg = "Ugh, its a Monday morning!";
-        } else if (day == 2 || day == 3 || day == 4) {
-            msg = "Good Morning";
-        } else if (day == 5) {
-            msg = "Its finally Friday!! Goodmorning :)";
-        }
-    } else if (hour > 17 ) {
-        msg = "Good Evening"
-     } else if (day == 6){
-         msg = "Enjoy you evening...tomorrow is Monday"
-    }
-    else if (hour < 15 ) {
-        msg = "Good Day!"
-    }
-
-    return msg;
-}
 
