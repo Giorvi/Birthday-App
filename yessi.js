579999a7f21e9b59008bd4b5c8a3aad92482c508
Yessenia Hernandez Hernandez <yh90926@srcs.loc>
2018-02-26 11:10:31 -0800

    working on countdown and sign fucntion

--- yessi.js
+++ yessi.js
@@ -1,70 +1,68 @@
-var button = document.getElementById("submitbirthday");
+var button = document.getElementById("button2");
 button.addEventListener("click", () => {
-    document.getElementById('output').textContent = getAge()
+    document.getElementById('output').textContent = birthdaycountdown()
 
 });
 
 
+function birthdayCountDown() {
+    var today = new Date,
+        Nextyear = today.getYear() + 1,
+        Birthday = document.getElementById(“birthdayinput”).value,
+        Birthdate = new Date(birthday),
+        Nextbirthday = new Date(birthdate.setFullYear(today.getFullYear()));
+}
+var daysuntilnextbirthday = Math.ceil((nextbirthday - today) / (1000 * 60 * 60 * 24));
+
+return daysuntilnextbirthday
+}
+
 function getSign() {
     var today = new Date,
-        birthday = document.getElementById("birthdayinput").value,
-        birthdate = new Date(birthday),
-        days = birthdate.getDate(),
-    month = birthdate.getMonth();
-    if ((month() == 2 && sign.getDate() >= 21 || sign.getMonth() == 3 && sign.getDate() <= 19)) {
+        Birthday = document.getElementById("birthdayinput").value,
+        Birthdate = new Date(birthday),
+        Days = birthdate.getDate(),
+        Month = birthdate.getMonth();
+
+    if ((month == 2 && days >= 21 || month == 3 && days <= 19)) {
         return "Aries";
 
-    } else
-    if ((sign.getMonth() == 3 && sign.getDate() >= 20 || sign.getMonth() == 4 && sign.getDate() <= 20)) {
+    } else if ((month == 3 && days >= 20 || month == 4 && days <= 20)) {
         return "Taurus";
 
-    } else if ((sign.getMonth() == 4 && sign.getDate() >= 21 || sign.getMonth() == 5 && sign.getDate() <= 20)) {
+    } else if ((month == 4 && days >= 21 || month == 5 && days <= 20)) {
         return "Gemini";
 
-    } else if ((sign.getMonth() == 5 && sign.getDate() >= 21 || sign.getMonth() == 6 && sign.getDate() <= 22)) {
+    } else if ((month == 5 && days >= 21 || month == 6 && days <= 22)) {
         return "Cancer";
 
-    } else if ((sign.getMonth() == 6 && sign.getDate() >= 23 || sign.getMonth() == 7 && sign.getDate() <= 22)) {
+    } else if ((month == 6 && days >= 23 || month == 7 && days <= 22)) {
         return "Leo";
 
-    } else if ((sign.getMonth() == 7 && sign.getDate() >= 23 || sign.getMonth() == 8 && sign.getDate() <= 22)) {
+    } else if ((month == 7 && days >= 23 || month == 8 && days <= 22)) {
         return "Virgo";
 
-    } else if ((sign.getMonth() == 8 && sign.getDate() >= 23 || sign.getMonth() == 9 && sign.getDate() <= 22)) {
+    } else if ((month == 8 && days >= 23 || month == 9 && days <= 22)) {
         return "Libra";
 
-    } else if ((sign.getMonth() == 9 && sign.getDate() >= 23 || sign.getMonth() == 10 && sign.getDate() <= 21)) {
+    } else if ((month == 9 && days >= 23 || month == 10 && days <= 21)) {
         return "Scorpio";
-    } else if ((sign.getMonth() == 10 && sign.getDate() >= 22 || sign.getMonth() == 11 && sign.getDate() <= 21)) {
+    } else if ((month == 10 && days >= 22 || month == 11 && days <= 21)) {
         return "Sagittarius";
 
-    } else if ((sign.getMonth() == 11 && sign.getDate() >= 22 || sign.getMonth() == 0 && sign.getDate() <= 19)) {
+    } else if ((month == 11 && days >= 22 || month == 0 && days <= 19)) {
         return "Capricorn";
 
-    } else if ((sign.getMonth() == 0 && sign.getDate() >= 20 || sign.getMonth() == 1 && sign.getDate() <= 18)) {
+    } else if ((month == 0 && days >= 20 || month == 1 && days <= 18)) {
         return "Aquarius";
 
 
-        return ("My Birthday is in " + days + " days :(");
+    } else if ((month == 1 && days >= 19 || month == 2 && days <= 20)) {
+        return "Pisces";
+
+
 
     }
 
-} else if ((sign.getMonth() == 1 && sign.getDate() >= 19 || sign.getMonth() == 2 && sign.getDate() <= 20)) {
-    return "Pisces";
 
 }
-}
-
-function birthdayCountDown(date) {
-    var today = new Date();
-    var myBirthday = new Date(date);
-    myBirthday.setFullYear(today.getFullYear());
-    if (today.getTime() > myBirthday.getTime()) {
-        myBirthday.setFullYear(today.getFullYear() + 1);
-    }
-    var diff = myBirthday.getTime() - today.getTime();
-    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-    return ("My Birthday is in " + days + " days :(");
-}
-
