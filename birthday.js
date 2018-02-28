ddf5a7b85845ca7c69c59189933480b1344cadee
Giorvi S Alvarez <ga73724@srcs.loc>
2018-02-28 11:33:19 -0800

    count down and get age are now work with one buton.

--- birthday.js
+++ birthday.js
@@ -9,9 +9,23 @@ function getAge() {
         birthdate = new Date(birthday),
         age = Math.floor((today - birthdate) / (365 * 24 * 60 * 60 * 1000));
 
-    return "You're are" + age + " years old";
+    return age;
 
 
+}
+function birthdayCountDown() {
+    var today = new Date,
+        nextyear = today.getYear() + 1,
+        birthday = document.getElementById("Birthday").value,
+        birthdate = new Date(birthday),
+        nextBirthday = new Date(birthdate.setFullYear(today.getFullYear()));
+    if (today - nextBirthday > 0){
+        var nextBirthday = nextBirthday.setFullYear(today.getFullYear()+ 1)
+
+    }
+var daysuntilnextbirthday = Math.ceil((nextBirthday - today) / (1000 * 60 * 60 * 24))
+
+return daysuntilnextbirthday;
 }
 
 function checkFormat() {
@@ -20,9 +34,11 @@ function checkFormat() {
     var check2 = format.substring(5, 6);
     if ((check1 == "/") && (check2 == "/") && (format.length == 10)) {
 
-            return getAge();
+            return "You are " + getAge() + " years old and there are " +
+birthdayCountDown() + " days till your birthday";
 
     } else {
         return "Sorry, but you did not enter a date in the correct format of mm/dd/yyyy.";
     }
 }
+
