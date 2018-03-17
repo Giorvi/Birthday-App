19a5a9f28e996b1fea06ab2ead3c98f00d728a6d
Giorvi Alvarez <giorvialvarez@giorvis-mbp.attlocal.net>
2018-03-17 16:44:46 -0700

    comments

--- birthday.js
+++ birthday.js
@@ -1,11 +1,9 @@
-// Button
 var button = document.getElementById("submitBirthday");
 button.addEventListener("click", () => {
     document.getElementById('output').textContent = checkFormat();
 });
 
-// getAge function.
-// Takes Birtdate input and returns how old the prson is based on the input.
+
 function getAge() {
     var today = new Date(),
         birthday = document.getElementById("Birthday").value,
@@ -17,8 +15,7 @@ function getAge() {
 
 }
 
-// birthdayCountDoen funtion.
-// based on the input by the user return a count down untill their next birthday.
+
 function birthdayCountDown() {
     var today = new Date,
         nextyear = today.getYear() + 1,
@@ -34,8 +31,7 @@ function birthdayCountDown() {
     return daysuntilnextbirthday;
 }
 
-// getsign function.
-// based on user input return their astrological sign.
+
 function getSign() {
     var today = new Date;
     var birthday = document.getElementById("Birthday").value;
@@ -80,10 +76,6 @@ function getSign() {
     }
 }
 
-// checkFomat function
-/*this funtion makes sure that the inout from the user is in the correct date format.
-The funtion checks for certain permaiter and then returns on of the two things. It can return getAge,
-birthdayCountDown, and get sign or you didn't enter the correct format*/
 function checkFormat() {
     var format = document.getElementById("Birthday").value;
     var check1 = format.substring(2, 3);
